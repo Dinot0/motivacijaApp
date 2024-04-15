41aa47a22e86283aa5c2fc1a711d776018e947d4
Dino <tiesko.dino@gmail.com>
2024-04-15 18:21:37 +0200

    Initial app

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/app.js', // Ulazna točka - glavna JavaScript datoteka                ovu putanju popravit
+  output: {
+    filename: 'bundle.js', // Naziv izlazne bundle datoteke
+    path: path.resolve(__dirname, 'dist'), // Putanja do izlaznog direktorija
+  },
+  mode: "none"
+};
